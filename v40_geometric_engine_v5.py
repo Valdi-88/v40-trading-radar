8dcc7edf55986e14949ab1e2ecc72d56780f6df9
Valdi-88 <kratik0905@gmail.com>
2026-09-18 17:27:21 +0530

    Fix column widths assignment in geometric engine

--- v40_geometric_engine_v5.py
+++ v40_geometric_engine_v5.py
@@ -526,7 +526,7 @@ def create_geometric_workbook(ticker_list, output_filename="v40_geometric_analys
 
             current_row += 1
 
-    col_widths =
+    col_widths = [18, 38, 14, 18, 20, 32, 18, 55, 32]
     for i, w in enumerate(col_widths, start=1):
         ws.column_dimensions[get_column_letter(i)].width = w
 
